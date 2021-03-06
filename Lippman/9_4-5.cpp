c403c24d02956154fa11ae9c1782e42f292a5f7f
Kirin Pavel <blustershow@gmail.com>
2021-03-06 22:45:31 +0300

    Lippman 9_{4,5} done.

---
+++ Lippman/9_4-5.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+/* Exercise 9.4 
+** Search element in vector<int> between <begin> and <end> iterators.
+** Returns True or False.
+*/
+bool bool_search(std::vector<int>::iterator begin,
+					std::vector<int>::iterator end,
+					int to_find)
+{
+	while (begin != end)
+	{
+		if (*begin == to_find)
+			return true;
+		begin++;
+	}
+	return false;
+}
+
+/* Exercise 9.5 
+** Search element in vector<int> between <begin> and <end> iterators.
+** If element does not exist -- functions returns .end() iterator.
+*/
+std::vector<int>::iterator it_search(std::vector<int>::iterator begin,
+										std::vector<int>::iterator end,
+										int to_find)
+{
+	while (begin != end)
+	{
+		if (*begin == to_find)
+			break;
+		begin++;
+	}
+	return begin;
+}
